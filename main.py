e7ab8ff32e5de4c872f8fb7523a80c50e0927562
NE-Ninny <neninny@proton.me>
2025-05-27 06:57:03 -0400

    added .gitignore and functionality for main.py to read and write book file

---
+++ main.py
@@ -0,0 +1,13 @@
+def get_book_text(file_path):
+    with open(file_path) as f:
+        file_contents = f.read()
+        
+        return file_contents
+        # print(file_contents)
+
+def main():    
+    frankenstein = get_book_text("./books/frankenstein.txt")
+
+    print(frankenstein)
+
+main()
