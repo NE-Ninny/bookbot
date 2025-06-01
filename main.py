91f175d1c102381ef40e6a009b174148f35a1d46
NE-Ninny <neninny@proton.me>
2025-06-01 10:33:07 -0400

    add __pycache__ to .gitignore

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import num_of_words
+from stats import character_count
 
 # def get_book_text(filepath):
 #     with open(filepath) as f:
@@ -18,7 +19,9 @@ from stats import num_of_words
 
 def main():    
     word_count = num_of_words("./books/frankenstein.txt")
+    num_of_characters = character_count("./books/frankenstein.txt")
 
     print(word_count)
+    print(num_of_characters)
 
 main()
--- stats.py
+++ stats.py
@@ -12,3 +12,21 @@ def num_of_words(filepath):
         word_count += 1
 	
     return f"{word_count} words found in the document"
+
+def character_count(filepath):
+    lower_case_text = get_book_text(filepath).lower()
+    character_dict = {
+        "a": 0, "b": 0, "c": 0, "d": 0, "e": 0,
+        "f": 0, "g": 0, "h": 0, "i": 0, "j": 0,
+        "k": 0, "l": 0, "m": 0, "n": 0, "o": 0,
+        "p": 0, "q": 0, "r": 0, "s": 0, "t": 0,
+        "u": 0, "v": 0, "w": 0, "x": 0, "y": 0,
+        "z": 0, " ": 0
+    }
+
+    for text in lower_case_text.split():
+        text_length = len(text) + 1
+
+        print(text_length)
+        
+        # print(text)
