5c687d971b9fbe0c13b345f05c19eedcabe8749c
ColinKnox <colinwknox@gmail.com>
2025-06-12 10:52:08 -0400

    have the char and num dicts print separately

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import num_of_words
 from stats import character_count
+from stats import sorted_char
 
 def main():    
     word_count = num_of_words("./books/frankenstein.txt")
@@ -10,7 +11,7 @@ def main():
     print("----------- Word Count ----------")
     print(word_count)
     print("--------- Character Count -------")
-    # print(num_of_characters)
+    print(sorted_char(character_count("./books/frankenstein.txt")))
     print("============= END ===============")
 
 main()
